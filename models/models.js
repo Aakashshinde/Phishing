819032c495c3115d035c0e7a78eefea7f58f2416
Aakashshinde <rajshindeslv@gmail.com>
2021-02-21 14:05:23 +0000

    deleted

--- models/models.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const userdata = new mongoose.Schema({
-    email: String,
-    password: String,
-})  
-
-const user = mongoose.model('userdata',userdata)
-module.exports = user
